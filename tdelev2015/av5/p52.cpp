9bf9e9cc9c04748c8cd9dceab3a0dd1f134233e3
tdelev <tomce.delev@gmail.com>
2015-03-23 21:00:45 +0100

    Fixed indentation

--- tdelev2015/av5/p52.cpp
+++ tdelev2015/av5/p52.cpp
@@ -4,125 +4,125 @@ using namespace std;
 
 class Array {
 private:
-	int *a;
-	int size;
-	int count;
+  int *a;
+  int size;
+  int count;
 public:
-	// constructor
-	Array(int size = 10) {
-		count = 0;
-		this->size = size;
-		a = new int[size];
-	}
+  // constructor
+  Array(int size = 10) {
+    count = 0;
+    this->size = size;
+    a = new int[size];
+  }
 
-	// copy constructor
-	Array(const Array &x) {
-		copy(x);
-	}
+  // copy constructor
+  Array(const Array &x) {
+    copy(x);
+  }
 
-	// destructor
-	~Array() {
-		delete [] a;
-	}
+  // destructor
+  ~Array() {
+    delete [] a;
+  }
 
-	// asignment operator =
-	Array& operator=(const Array &x) {
-		if (this == &x) return *this;
-		delete [] a;
-		copy(x);
-		return *this;
-	}
+  // asignment operator =
+  Array& operator=(const Array &x) {
+    if (this == &x) return *this;
+    delete [] a;
+    copy(x);
+    return *this;
+  }
 
-	void copy(const Array &x) {
-		a = new int[x.size];
-		size = x.size;
-		count = x.count;
-		for (int i = 0; i < size; ++i) {
-			a[i] = x.a[i];
-		}
-	}
+  void copy(const Array &x) {
+    a = new int[x.size];
+    size = x.size;
+    count = x.count;
+    for (int i = 0; i < size; ++i) {
+      a[i] = x.a[i];
+    }
+  }
 
-	void add(int x) {
-		if (count == size) {
-			cout << "Allocation : " << size << endl;
-			int *t = a;
-			// Solution number 1
-			//a = new int[size + 1];
-			//size++;
-			// Solution number 2
-			a = new int[2 * size];
-			size *= 2;
-			for (int i = 0; i < count; ++i) {
-				a[i] = t[i];
-			}
-			delete [] t;
-		}
-		a[count] = x;
-		count++;
-	}
-	Array& operator>(int x) {
-		if (count == size) {
-			cout << "Allocation : " << size << endl;
-			int *t = a;
-			// Solution number 1
-			//a = new int[size + 1];
-			//size++;
-			// Solution number 2
-			a = new int[2 * size];
-			size *= 2;
-			for (int i = 0; i < count; ++i) {
-				a[i] = t[i];
-			}
-			delete [] t;
-		}
-		a[count] = x;
-		count++;
-		return *this;
-	}
+  void add(int x) {
+    if (count == size) {
+      cout << "Allocation : " << size << endl;
+      int *t = a;
+      // Solution number 1
+      //a = new int[size + 1];
+      //size++;
+      // Solution number 2
+      a = new int[2 * size];
+      size *= 2;
+      for (int i = 0; i < count; ++i) {
+        a[i] = t[i];
+      }
+      delete [] t;
+    }
+    a[count] = x;
+    count++;
+  }
+  Array& operator>(int x) {
+    if (count == size) {
+      cout << "Allocation : " << size << endl;
+      int *t = a;
+      // Solution number 1
+      //a = new int[size + 1];
+      //size++;
+      // Solution number 2
+      a = new int[2 * size];
+      size *= 2;
+      for (int i = 0; i < count; ++i) {
+        a[i] = t[i];
+      }
+      delete [] t;
+    }
+    a[count] = x;
+    count++;
+    return *this;
+  }
 
-	int get(int index) {
-		return a[index];
-	}
+  int get(int index) {
+    return a[index];
+  }
 
-	void set(int index, int value) {
-		a[index] = value;
-	}
+  void set(int index, int value) {
+    a[index] = value;
+  }
 
-	int& operator[](int index) {
-		return a[index];
-	}
+  int& operator[](int index) {
+    return a[index];
+  }
 
-	int getCount() {
-		return count;
-	}
+  int getCount() {
+    return count;
+  }
 
-	void show() {
-		cout << "Count: " << count << endl;
-		cout << "Size: " << size << endl;
-		for (int i = 0; i < count; ++i) {
-			cout << (*this)[i] << "\t";
-		}
-		cout << endl;
-	}
+  void show() {
+    cout << "Count: " << count << endl;
+    cout << "Size: " << size << endl;
+    for (int i = 0; i < count; ++i) {
+      cout << (*this)[i] << "\t";
+    }
+    cout << endl;
+  }
 };
 
 int main() {
-	Array x(5);
-	Array y;
-	int a;
-	/*while (true) {
-		cin >> a;
-		if (a < 0) break;
-		x.add(a);
-	}*/
-	for (int i = 0; i < 10; ++i) {
-		x > (rand() % 100);
-	}
-	x[5] = 100000;
-	x.show();
-	y = x;
-	x.set(4, 99);
-	x.show();
-	y.show();
-	return 0;
+  Array x(5);
+  Array y;
+  int a;
+  /*while (true) {
+  	cin >> a;
+  	if (a < 0) break;
+  	x.add(a);
+  }*/
+  for (int i = 0; i < 10; ++i) {
+    x > (rand() % 100);
+  }
+  x[5] = 100000;
+  x.show();
+  y = x;
+  x.set(4, 99);
+  x.show();
+  y.show();
+  return 0;
 }
